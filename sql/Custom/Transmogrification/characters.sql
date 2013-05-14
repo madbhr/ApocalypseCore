d9a6fcb4fc4e044f0b84a17c8d868bf71e2479a8
One <projectonecore@gmail.com>
2013-05-14 12:56:21 -0600

    delete custom sql

--- sql/Custom/Transmogrification/characters.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `custom_transmogrification` (
-	`GUID` INT(10) UNSIGNED NOT NULL COMMENT 'Item guidLow',
-	`FakeEntry` INT(10) UNSIGNED NOT NULL COMMENT 'Item entry',
-	`Owner` INT(10) UNSIGNED NOT NULL COMMENT 'Player guidLow',
-	PRIMARY KEY (`GUID`)
-)
-COMMENT='version 4.0'
-COLLATE='latin1_swedish_ci'
-ENGINE=InnoDB;
