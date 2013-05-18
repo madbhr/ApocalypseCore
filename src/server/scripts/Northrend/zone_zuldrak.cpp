0a83ad234b29b0a1e11975eb1db3dd91b7793adb
One <projectonecore@gmail.com>
2013-05-18 09:31:27 -0600

    Core: Add "GetRaceNameByID" and "GetClassNameByID" to get localized c…
    …lass and race names.
    
    Core: Add "GetRaceNameByID" and "GetClassNameByID" to get localized
    class and race names.

--- src/server/scripts/Northrend/zone_zuldrak.cpp
+++ src/server/scripts/Northrend/zone_zuldrak.cpp
@@ -644,7 +644,7 @@ public:
             events.ScheduleEvent(EVENT_WHIRLWIND, 15000);
         }
 
-        void UpdateAI(const uint32 diff)
+        void UpdateAI(uint32 diff)
         {
             if (!UpdateVictim())
                 return;
