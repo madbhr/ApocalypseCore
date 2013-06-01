da73a49818304ac1e9690937d070787997f6659a
One <projectonecore@gmail.com>
2013-06-01 14:34:16 -0600

    Core/Scripts: Always run OnSpellClick AI hook, even if there are no s…
    …pells defined for spellclick event.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -16807,12 +16807,9 @@ bool Unit::HandleSpellClick(Unit* clicker, int8 seatId)
         result = true;
     }
 
-    if (result)
-    {
-        Creature* creature = ToCreature();
-        if (creature && creature->IsAIEnabled)
-            creature->AI()->OnSpellClick(clicker);
-    }
+    Creature* creature = ToCreature();
+    if (creature && creature->IsAIEnabled)
+        creature->AI()->OnSpellClick(clicker, result);
 
     return result;
 }
