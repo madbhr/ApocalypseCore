5ca1c05a6172cfec53340edc30de21beb381427c
One <projectonecore@gmail.com>
2013-06-05 08:40:46 -0600

    Actualizando Sql, quitando todo sql custom y re-ordenando toda la carpeta

--- sql/updates/AC-update/AntiCheat/AntiCheat Pasivo/2013_04_10_world_AntiCheat_string.sql
+++ sql/updates/AC-update/AntiCheat/AntiCheat Pasivo/2013_04_10_world_AntiCheat_string.sql
@@ -1,4 +1,3 @@
-
--- Add AutoBanned Passive Anticheat System!
-REPLACE INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
-(11002, '|cFFFFFC00[ANTICHEAT]|cFF00FFFF[|cFF60FF00%s|cFF00FFFF] Baneado por Tramposo!|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+DELETE FROM `trinity_string` WHERE `entry` = 11002;
+INSERT INTO `trinity_string` (`entry`, `content_default`) VALUES
+(11002, '|cFFFFBF00[%s]:|cFFFFFFFF [%s] |cFF00FFFFdetected as possible cheater.');
