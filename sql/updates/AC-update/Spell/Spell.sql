0dce2437a43b07cf205931997bbf3047868f9633
One <projectonecore@gmail.com>
2013-06-05 10:01:20 -0600

    Ordenando mas Sql

--- sql/updates/AC-update/Spell/Spell.sql
+++ sql/updates/AC-update/Spell/Spell.sql
@@ -10,3 +10,15 @@ INSERT INTO spell_script_names VALUES ('73331', 'spell_item_sylvanas_music_box')
 DELETE FROM `spell_script_names` WHERE `spell_id`=6358;
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (6358,'spell_warl_seduction');
+
+-- Blessing of Sanctuary vs Vigilance
+DELETE FROM `spell_dbc` WHERE `id` = 20912;
+INSERT INTO `spell_dbc` (`Id`,`CastingTimeIndex`,`DurationIndex`,`RangeIndex`,`Effect1`,`EffectBasePoints1`,`EffectImplicitTargetA1`,`EffectApplyAuraName1`,`EffectMiscValue1`,`SpellFamilyName`,`Comment`) VALUES
+(20912,1,21,1,6,-3,1,87,127,10,'Blessing of Sanctuary Helper');
+
+UPDATE `spell_group` SET `spell_id` = 68066 WHERE `id` = 1091 and `spell_id` = 47930;
+UPDATE `spell_group` SET `spell_id` = 20912 WHERE `id` = 1092 and `spell_id` = 20911;
+
+-- spell_warl_shadow_bite
+DELETE FROM spell_script_names WHERE spell_id IN (-54049,54049,54050,54051,54052,54053);
+INSERT INTO spell_script_names (spell_id, ScriptName) VALUES (-54049, 'spell_warl_shadow_bite');
