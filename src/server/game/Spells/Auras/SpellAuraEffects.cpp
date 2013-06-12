1b6bdc200b178a12fd895518d316b9bd657ed088
One <projectonecore@gmail.com>
2013-06-12 13:29:38 -0600

    DB/Quests: Fix Badlands Reagent Run II
    Thanks @joschiwald and @Kirkhammett
    
    -trinitycore

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -5552,10 +5552,6 @@ void AuraEffect::HandlePeriodicTriggerSpellAuraTick(Unit* target, Unit* caster) 
             {
                 switch (auraId)
                 {
-                    // Thaumaturgy Channel
-                    case 9712:
-                        triggerSpellId = 21029;
-                        break;
                     // Brood Affliction: Bronze
                     case 23170:
                         triggerSpellId = 23171;
