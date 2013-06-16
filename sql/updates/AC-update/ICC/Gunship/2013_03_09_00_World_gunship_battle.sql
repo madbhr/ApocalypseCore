af3a6c83de63ac282c88310ff2d38cd36e2f936b
One <projectonecore@gmail.com>
2013-06-16 16:55:16 -0600

    fixed GunShip battle Sql

--- sql/updates/AC-update/ICC/Gunship/2013_03_09_00_World_gunship_battle.sql
+++ sql/updates/AC-update/ICC/Gunship/2013_03_09_00_World_gunship_battle.sql
@@ -42,8 +42,6 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_sergeant' WHERE `entry` = 369
 UPDATE `creature_template` SET `ScriptName` = 'npc_sergeant' WHERE `entry` = 36961;
 UPDATE `creature_template` SET `ScriptName` = 'npc_gunship_skybreaker' WHERE `entry` = 37540;
 UPDATE `creature_template` SET `ScriptName` = 'npc_gunship_orgrimmar' WHERE `entry` = 37215;
-UPDATE `creature_template` SET `ScriptName` = 'npc_gunship_cannon' WHERE `entry` = 36838;
-UPDATE `creature_template` SET `ScriptName` = 'npc_gunship_cannon' WHERE `entry` = 36839;
 UPDATE `gameobject_template` SET `ScriptName` = 'transport_gunship' WHERE `entry` IN (201580, 201812);
 UPDATE `item_template` SET ScriptName = 'item_icc_rocket_pack' WHERE entry = 49278;
 
@@ -70,24 +68,6 @@ DELETE FROM transports WHERE entry IN (201580, 201581, 201811, 201812);
 DELETE FROM creature WHERE id IN (37230, 36838, 36839, 36948, 36939);
 DELETE FROM `creature_template_addon` WHERE (`entry`=37547);
 UPDATE `creature_template` SET `flags_extra` = 130 WHERE `entry` = 37547;
-
--- Cannon template
-UPDATE `creature_template` SET `exp` = 2, `VehicleId` = 554, `RegenHealth` = 0, `npcflag` = 16777216, `spell1` = 69399, `spell2` = 69401 WHERE entry = 36839;
-UPDATE `creature_template` SET `exp` = 2, `VehicleId` = 554, `RegenHealth` = 0, `npcflag` = 16777216, `spell1` = 70172, `spell2` = 70174 WHERE entry = 36838;
-DELETE FROM `npc_spellclick_spells` WHERE npc_entry IN (36838, 36839);
-INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`) VALUES
-('36838', '46598', '1'),
-('36839', '46598', '1');
-DELETE FROM `creature_template_addon` WHERE `entry` IN (36838, 36839);
-INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
-(36838, 0, 0, 0, 1, 0, 69470),
-(36839, 0, 0, 0, 1, 0, 69470);
-UPDATE `creature_template` SET `difficulty_entry_1` = '38129',`difficulty_entry_2` = '38701', `difficulty_entry_3` = '38702' WHERE `entry` =37215;
-UPDATE `creature_template` SET `faction_A` = 35, `faction_H` = 35, `minlevel` = 83, `maxlevel` = 83, `unit_flags` = 4, `flags_extra` = 2, `modelid1` = 1126, `modelid2` = 11686 WHERE `entry` IN (37215, 38129, 38701, 38702, 37540, 38128, 38699, 38700);
-
--- Fix for Cannons
-UPDATE `creature_template` SET `difficulty_entry_1`=0 WHERE `entry`=36839 LIMIT 1;
-UPDATE `creature_template` SET `speed_walk`=0 ,`speed_run`=0 WHERE `entry` IN (36839,36838);
 
 -- Spell effects
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 71193 AND `spell_effect` = -71188;
@@ -132,7 +112,7 @@ SET @VINDICATOR := 37003;
 SET @SORCERER := 37026;
 SET @PROTECTOR := 36998;
 SET @FROSTWYRM := 37230;
-SET @SOUNDID :=0;
+SET @SOUNDID := 0;
 
 DELETE FROM `creature_text` WHERE `entry` IN (36948, 36939, 37030, 37033, 37032, 37003, 37026, 36998, 37230, 50004, 50006);
 INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
@@ -394,8 +374,12 @@ UPDATE `creature_template` SET `unit_flags`=2, `flags_extra`=2 WHERE `entry`=386
 UPDATE `item_template` SET `area` = 4812 WHERE `entry` = 49278;
 UPDATE `item_template` SET `map` = 631 WHERE `entry` = 49278;
 
+
+
+-- Fix for Gunship Battle Cannons
 UPDATE `creature_template` SET `ScriptName` = 'npc_gunship_cannon' WHERE `entry` = 36838;
 UPDATE `creature_template` SET `ScriptName` = 'npc_gunship_cannon' WHERE `entry` = 36839;
+
 -- Cannon template
 UPDATE `creature_template` SET `exp` = 2, `VehicleId` = 554, `RegenHealth` = 0, `npcflag` = 16777216, `spell1` = 69399, `spell2` = 69401 WHERE entry = 36839;
 UPDATE `creature_template` SET `exp` = 2, `VehicleId` = 554, `RegenHealth` = 0, `npcflag` = 16777216, `spell1` = 70172, `spell2` = 70174 WHERE entry = 36838;
