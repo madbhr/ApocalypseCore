5863adb9d0d193c133cef9f37041849083c56573
One <projectonecore@gmail.com>
2013-06-16 12:35:43 -0600

    Scripting: Update Lake Frog Script
    - TrinityCore

--- src/server/scripts/Northrend/zone_grizzly_hills.cpp
+++ src/server/scripts/Northrend/zone_grizzly_hills.cpp
@@ -698,135 +698,138 @@ class npc_venture_co_straggler : public CreatureScript
 ## Quest A Blade Fit For A Champion
 ######*/
 
-enum eLakeFrog
+enum LakeFrog
 {
+    // Spells
     SPELL_WARTSBGONE_LIP_BALM              = 62574,
     SPELL_FROG_LOVE                        = 62537, // for 1 minute !
     SPELL_WARTS                            = 62581,
     SPELL_MAIDEN_OF_ASHWOOD_LAKE_TRANSFORM = 62550,
     SPELL_SUMMON_ASHWOOD_BRAND             = 62554,
+
+    // Items
     ITEM_WARTS_B_GONE_LIP_BALM             = 44986,
+
+    // Creature
     NPC_LAKE_FROG                          = 33211,
     NPC_LAKE_FROG_QUEST                    = 33224,
+    NPC_MAIDEN_OF_ASHWOOD_LAKE             = 33220,
+
+    // Text
     SAY_MAIDEN_0                           = 0,
     SAY_MAIDEN_1                           = 1
+};
+
+enum LakeFrogEvents
+{
+    EVENT_SCRIPT_1                         = 1,
+    EVENT_SCRIPT_2                         = 2,
+    EVENT_SCRIPT_3                         = 3,
+    EVENT_SCRIPT_4                         = 4,
+    EVENT_SCRIPT_5                         = 5
 };
 
 class npc_lake_frog : public CreatureScript
 {
     public:
-        npc_lake_frog(): CreatureScript("npc_lake_frog") { }
+        npc_lake_frog() : CreatureScript("npc_lake_frog") { }
 
-    struct npc_lake_frogAI : public ScriptedAI
-    {
-        npc_lake_frogAI(Creature* creature) : ScriptedAI(creature) { }
-
-        void Reset()
+        struct npc_lake_frogAI : public ScriptedAI
         {
-            uiFollowing = false;
-            uiRunningScript = false;
-            uiPhase = 0;
-            if (me->GetEntry() == NPC_LAKE_FROG_QUEST)
-                me->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
-        }
+            npc_lake_frogAI(Creature* creature) : ScriptedAI(creature) { }
 
-        void UpdateAI(uint32 uiDiff)
-        {
-            if (uiFollowing)
-                if(!me->HasAura(SPELL_FROG_LOVE))
-                    me->DespawnOrUnsummon(0);
-
-            if (uiRunningScript)
+            void Reset()
             {
-                if (uiScriptTimer <= uiDiff)
+                _following = false;
+                _runningScript = false;
+                if (me->GetEntry() == NPC_LAKE_FROG_QUEST)
+                    me->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
+            }
+
+            void UpdateAI(uint32 diff)
+            {
+                if (_following)
+                    if (!me->HasAura(SPELL_FROG_LOVE))
+                        me->DespawnOrUnsummon(1000);
+
+                _events.Update(diff);
+
+                while (uint32 eventId = _events.ExecuteEvent())
                 {
-                    switch (uiPhase)
+                    switch (eventId)
                     {
-                        case 0:
+                        case EVENT_SCRIPT_1:
                             DoCast(me, SPELL_MAIDEN_OF_ASHWOOD_LAKE_TRANSFORM);
-                            me->SetEntry(33220);
-                            uiScriptTimer = 2000;
-                            ++uiPhase;
+                            me->SetEntry(NPC_MAIDEN_OF_ASHWOOD_LAKE);
+                            _events.ScheduleEvent(EVENT_SCRIPT_2, 2000);
                             break;
-                        case 1:
+                        case EVENT_SCRIPT_2:
                             Talk(SAY_MAIDEN_0);
-                            uiScriptTimer = 3000;
-                            ++uiPhase;
+                            _events.ScheduleEvent(EVENT_SCRIPT_3, 3000);
                             break;
-                        case 2:
+                        case EVENT_SCRIPT_3:
                             me->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
-                            uiScriptTimer = 25000;
-                            ++uiPhase;
+                            _events.ScheduleEvent(EVENT_SCRIPT_4, 25000);
                             break;
-                        case 3:
+                        case EVENT_SCRIPT_4:
                             me->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
-                            uiScriptTimer = 2000;
-                            ++uiPhase;
+                            _events.ScheduleEvent(EVENT_SCRIPT_5, 2000);
                             break;
-                        case 4:
+                        case EVENT_SCRIPT_5:
                             Talk(SAY_MAIDEN_1);
-                            uiScriptTimer = 4000;
-                            ++uiPhase;
+                            me->DespawnOrUnsummon(4000);
                             break;
-                        case 5:
-                            uiRunningScript=false;
-                            me->DespawnOrUnsummon(0);
+                        default:
                             break;
                     }
                 }
-                else if (uiScriptTimer)
-                    uiScriptTimer -= uiDiff;
             }
 
-        }
-
-        void ReceiveEmote(Player* player, uint32 emote)
-        {
-            if (uiFollowing || uiRunningScript)
-                return;
-
-            if(emote==TEXT_EMOTE_KISS && me->IsWithinDistInMap(player, 30.0f) && player->HasItemCount(ITEM_WARTS_B_GONE_LIP_BALM, 1, false))
+            void ReceiveEmote(Player* player, uint32 emote)
             {
-                if(!player->HasAura(SPELL_WARTSBGONE_LIP_BALM))
-                    player->AddAura(SPELL_WARTS, player);
+                if (_following || _runningScript)
+                    return;
 
-                else
+                if (emote == TEXT_EMOTE_KISS && me->IsWithinDistInMap(player, 30.0f) && player->HasItemCount(ITEM_WARTS_B_GONE_LIP_BALM, 1, false))
                 {
-                    player->RemoveAura(SPELL_WARTSBGONE_LIP_BALM);
+                    if (!player->HasAura(SPELL_WARTSBGONE_LIP_BALM))
+                        player->AddAura(SPELL_WARTS, player);
+                    else
+                    {
+                        player->RemoveAura(SPELL_WARTSBGONE_LIP_BALM);
 
-                    if (me->GetEntry() == NPC_LAKE_FROG)
-                    {
-                        me->AddAura(SPELL_FROG_LOVE, me);
-                        me->GetMotionMaster()->MoveFollow(player, 0.3f, frand (M_PI/2, M_PI + (M_PI/2)));
-                        uiFollowing=true;
-                    }
-                    else if (me->GetEntry() == NPC_LAKE_FROG_QUEST)
-                    {
-                        me->GetMotionMaster()->MoveIdle();
-                        me->SetFacingToObject(player);
-                        uiRunningScript=true;
-                        uiScriptTimer = 2000;
+                        if (me->GetEntry() == NPC_LAKE_FROG)
+                        {
+                            me->AddAura(SPELL_FROG_LOVE, me);
+                            me->GetMotionMaster()->MoveFollow(player, 0.3f, frand(M_PI/2, M_PI + (M_PI/2)));
+                            _following = true;
+                        }
+                        else if (me->GetEntry() == NPC_LAKE_FROG_QUEST)
+                        {
+                            me->GetMotionMaster()->MoveIdle();
+                            me->SetFacingToObject(player);
+                            _runningScript = true;
+                            _events.ScheduleEvent(EVENT_SCRIPT_1, 2000);
+                        }
                     }
                 }
             }
-        }
 
-        void sGossipSelect(Player* player, uint32 /*sender*/, uint32 /*action*/)
+            void sGossipSelect(Player* player, uint32 /*sender*/, uint32 /*action*/)
+            {
+                DoCast(player, SPELL_SUMMON_ASHWOOD_BRAND);
+            }
+
+        private:
+            EventMap _events;
+            bool   _following;
+            bool   _runningScript;
+        };
+
+        CreatureAI* GetAI(Creature* creature) const
         {
-            DoCast(player, SPELL_SUMMON_ASHWOOD_BRAND);
+            return new npc_lake_frogAI(creature);
         }
-
-    private:
-        bool   uiFollowing;
-        bool   uiRunningScript;
-        uint32 uiScriptTimer;
-        uint8  uiPhase;
-    };
-
-    CreatureAI* GetAI(Creature* creature) const
-    {
-        return new npc_lake_frogAI(creature);
-    }
 };
 
 void AddSC_grizzly_hills()
@@ -841,3 +844,4 @@ void AddSC_grizzly_hills()
     new npc_venture_co_straggler();
     new npc_lake_frog();
 }
+
