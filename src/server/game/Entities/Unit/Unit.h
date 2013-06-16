a0eaca0227aab167588180cd1f1cf3fefed02909
One <projectonecore@gmail.com>
2013-06-16 13:44:19 -0600

    Core/Objects
    * Split BuildValuesUpdate into separate virtual methods for every object
    type that does something special with update fields
    * Removed _SetCreateBits/_SetUpdateBits to halve the amount of checks if
    field needs to be sent
    -TrinityCore

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -2150,6 +2150,8 @@ class Unit : public WorldObject
     protected:
         explicit Unit (bool isWorldObject);
 
+        void BuildValuesUpdate(uint8 updatetype, ByteBuffer* data, Player* target) const;
+
         UnitAI* i_AI, *i_disabledAI;
 
         void _UpdateSpells(uint32 time);
